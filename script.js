4a8be4a2cca3dc8b0577fc72a431f8307652ee48
Jimmy Thai <jvthai1@gmail.com>
2022-04-21 21:04:40 -0700

    Include web ui gameplay and logic

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function computerPlay(){
+window.onload=function(){
+  //scores
+  let playerScore = 0; 
+  let computerScore = 0; 
+  
+  
+  //display results
+  const results = document.querySelector('#results'); 
+  
+  //button selectors and click event listeners
+  const rock = document.querySelector('#rock');
+  const paper = document.querySelector('#paper');
+  const scissors = document.querySelector('#scissors');
+  
+  
+  rock.addEventListener('click', () => {
+    playRound("rock", computerPlay());
+    updateScore();
+    winnerCheck()
+  });
+  
+  scissors.addEventListener('click', () => {
+    playRound("scissors", computerPlay());
+    updateScore();
+    winnerCheck()
+  });
+  
+  paper.addEventListener('click', () => {
+    playRound("paper", computerPlay());
+    updateScore();
+    winnerCheck()
+  });
+  
+  
+  //display score
+  function updateScore(){
+    const score = document.querySelector('#score'); 
+    score.textContent = 'Player Score: ' + playerScore + ' Computer Score: ' + computerScore;
+  }
+  
+  //Computer's rps guess
+  function computerPlay(){
     //establish a way to randomize a result using Math.random
     let num = Math.random() * 100; 
       
@@ -15,47 +56,45 @@ function computerPlay(){
       
   }
   
+  
+  
+  //round playing logic
   function playRound(playerSelection, computerSelection){
   
     //long if else statements to dictate winner lol
     if (playerSelection == "rock" && computerSelection == "Scissors"){
-      return "Congratulations, Rock beats Scissors"
+      return "Rock beats Scissors, player win", playerScore++;
     }
     else if (playerSelection == "paper" && computerSelection == "Rock"){
-      return "Congratulations, Paper beats Rock"
+      return "Paper beats Rock, player win", playerScore++;
     }
     else if (playerSelection == "scissor" && computerSelection == "Paper"){
-      return "Congratulations, Scissor beats Paper"
+      return "Scissors beats Paper, player win", playerScore++;
     }
     else if (playerSelection == "scissors" && computerSelection == "Rock"){
-      return "You lose, Rock beats Scissors"
+      return "Scissors beats Rock, computer win", computerScore++; 
     }
     else if (playerSelection == "paper" && computerSelection == "Scissors"){
-      return "You lose, Scissors beats Paper"
+      return "Paper beats Scissors, computer win", computerScore++; 
     }
     else if (playerSelection == "rock" && computerSelection == "Paper"){
-      return "You lose, Paper beats Rock"
+      return "Rock beats paper, computer win", computerScore++; 
     }
     else {
-      return "It's a tie!"
+      return "Draw", computerScore++, playerScore++; 
     }
   }
   
-  function game(){
-    //loop the game play 5 times
-    let i = 0; 
-    while (i < 5){
-      console.log(playRound(getInput(),computerPlay()));
-      i++; 
+  function winnerCheck(){
+    if (playerScore == 5){
+      return results.textContent = "Player wins!"; 
+    }
+    else if (computerScore == 5){
+      return results.textContent = "Computer wins!"; 
+    }
+    else if (computerScore == 5 && playerScore == 5 ){
+      return results.textContent = "It's a draw!"; 
     }
   }
-  
-  function getInput(){
-    let playerGuess = window.prompt("Rock, paper, scissors, shoot!");
-  
-    if (playerGuess.toLowerCase() == "rock" || playerGuess.toLowerCase() == "paper" || playerGuess.toLowerCase() == "scissors" ){
-      return playerGuess.toLowerCase();
-    }
   }
   
-  game();
